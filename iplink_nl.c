40031cefa9e9900ea02f5f5157aca2b886ae780f
Albert Veli <albert.veli@gmail.com>
2020-04-29 14:33:39 +0200

    Add some more iplink info

--- iplink_nl.c
+++ iplink_nl.c
@@ -10,10 +10,31 @@
  * License: LGPLv2 (same as libnl)
  */
 #include <stdio.h>
+#include <string.h>
 #include <netlink/netlink.h>
 #include <netlink/route/addr.h>
 #include <netlink/route/link.h>
 #include <netlink/route/link/vlan.h>
+#include <netlink/route/link/bridge.h>
+
+/* Enough? */
+#define MAX_IFINDEX 256
+/* vids[ifindex] = vid */
+static int vids[MAX_IFINDEX];
+
+/* Return vlan ifindex for this vid, or 0 if not found. */
+static int vlan_ifindex(int vid)
+{
+	int i;
+
+	for (i = 1; i < MAX_IFINDEX; i++) {
+		if (vids[i] == vid)
+			return i;
+	}
+
+	/* Not found */
+	return 0;
+}
 
 /* If !free, alloc nl_sock else free previously allocated one */
 static struct nl_sock *cached_nl_sock(int free)
@@ -71,6 +92,19 @@ static void __link_cb(struct nl_object *obj, void *arg __attribute__((unused)))
 	const char *addr_s = nl_addr2str(addr, buf, sizeof(buf));
 	if (addr_s)
 		printf(", %s", addr_s);
+
+	if (rtnl_link_is_vlan(link)) {
+		int vid = rtnl_link_vlan_get_id(link);
+		if (ifindex >= 0 && ifindex < MAX_IFINDEX) {
+			vids[ifindex] = vid;
+			printf(" vid %d", vid);
+		}
+	}
+
+	if (rtnl_link_is_bridge(link)) {
+		printf(" bridge");
+	}
+
 	printf("\n");
 }
 
@@ -80,6 +114,8 @@ int print_links(void)
 	struct nl_cache *links;
 	int err;
 
+	memset(vids, 0, sizeof(vids));
+
 	err = rtnl_link_alloc_cache(sk, AF_UNSPEC, &links);
 	if (err) {
 		perror("rtnl_link_alloc_cache");
@@ -87,6 +123,7 @@ int print_links(void)
 	}
 	if (nl_cache_is_empty(links)) {
 		perror("nl cache empty");
+		nl_cache_free(links);
 		return 2;
 	}
 
@@ -94,16 +131,27 @@ int print_links(void)
 
 	/* Free allocated structs */
 	nl_cache_free(links);
-	cached_nl_sock(1);
 
 	return 0;
 }
 
-
 int main (void)
 {
-	//print_ifaces();
+	int vid, ifindex;
+
 	print_links();
+
+	/* Free allocated nl_sock */
+	cached_nl_sock(1);
+
+	/* Test if vlan_ifindex works.
+	 * This will only print something
+	 * if vlan interface with vid 1 exists.
+	 */
+	vid = 1;
+	ifindex = vlan_ifindex(vid);
+	if (ifindex > 0)
+		printf("\nifindex for vlan%d: %d\n", vid, ifindex);
 
 	return 0;
 }
